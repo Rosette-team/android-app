f8c8644c102897cd1a612f1d65efb566aaf675e6
FlamasterRu <60773749+FlamasterRu@users.noreply.github.com>
2022-12-09 19:00:28 +0300

    fix

--- app/src/main/java/com/example/android_app/MainActivity.kt
+++ app/src/main/java/com/example/android_app/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.android_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.activity_main)
     }
 }
