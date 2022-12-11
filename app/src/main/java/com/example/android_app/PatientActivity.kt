dafa6e5872fb0e61c6171bb2f95f801387d5e921
FlamasterRu <nikita172000@yandex.ru>
2022-12-11 16:07:23 +0300

    Create writeToDoctor activity

--- app/src/main/java/com/example/android_app/PatientActivity.kt
+++ app/src/main/java/com/example/android_app/PatientActivity.kt
@@ -1,11 +1,19 @@
 package com.example.android_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class PatientActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient)
+    }
+    fun writeToDoctorClicked(view: View) {
+        val writeToDoctorIntent = Intent(this, WriteToDoctor::class.java)
+        startActivity(writeToDoctorIntent)
     }
 }
