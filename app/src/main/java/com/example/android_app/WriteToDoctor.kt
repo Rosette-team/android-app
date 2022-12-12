ee9b6b786b8b5ee05b7b2b46954c5144b22faae1
FlamasterRu <60773749+FlamasterRu@users.noreply.github.com>
2022-12-12 22:28:25 +0300

    Fill WriteToDoctor

--- app/src/main/java/com/example/android_app/WriteToDoctor.kt
+++ app/src/main/java/com/example/android_app/WriteToDoctor.kt
@@ -1,11 +1,36 @@
 package com.example.android_app
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
 
 class WriteToDoctor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write_to_doctor)
+
+        val clinicList = arrayOf("Выбрать больницу", "Поликлиника №1", "Поликлиника №2", "Поликлиника №3")
+        val adapterClinic = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, clinicList)
+        adapterClinic.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        var spinnerC: Spinner = findViewById<Spinner>(R.id.spinnerClinic)
+        spinnerC.adapter = adapterClinic
+
+        val doctorCategoryList = arrayOf("Выбрать врача", "Терапевт", "Лор", "Хирург")
+        val adapterDoctorCategory = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, doctorCategoryList)
+        adapterDoctorCategory.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        var spinnerDC: Spinner = findViewById<Spinner>(R.id.spinnerDoctorCategory)
+        spinnerDC.adapter = adapterDoctorCategory
+
+        val doctorFIOList = arrayOf("Ф.И.О. врача", "Иванов И.И.", "Попов П.П.")
+        val adapterDoctorFIO = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, doctorFIOList)
+        adapterDoctorFIO.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        var spinnerDoctorFIO: Spinner = findViewById<Spinner>(R.id.spinnerDoctorFIO)
+        spinnerDoctorFIO.adapter = adapterDoctorFIO
+
     }
+
 }
